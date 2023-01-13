88399a0731987337689ebeb617a21543e0befd2b
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-01-14 00:20:22 +0200

    додає дз

--- homework.js/01-hw.js
+++ homework.js/01-hw.js
@@ -1,3 +1,13 @@
+const str = 'Привіт';
+console.log(typeof str);
+const number = 123;
+console.log(typeof number);
+const flag  = true;
+console.log(typeof flag);
+const txt = 'true';
+console.log(typeof txt)
+
+
 const myName = 'veronika';
 const Age = 15;
 const surname = 'Niema';
@@ -8,3 +18,45 @@ let friendsAge = 15;
 let numberfamilyMembers = 3;
 let numberOfSchoolGrade =10;
 let nameOfMyCat ="funtic";
+
+
+const answer = prompt ("Напишіть своє ім'я");
+console.log(answer);
+
+
+const w = 15;
+const n = 15;
+const message = (w + n);
+alert(message);
+
+
+const a = 5;
+const b = 6;
+console.log(a * b);
+
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+console.log(a1,a2,a3,a4);
+
+
+const a6 = 5 % 3;
+const a7 = 3 % 5;
+const a8 = 5 + '3';
+const a9 = '5' - 3;
+const a10 =  75 + 'кг';
+alert(a6,a7,a8,a9,a10)
+
+
+const width = 10;
+const height =23;
+const s = width * height;
+console.log(s);
+
+
+const heightC = 10;
+const dC = 4;
+const v = heightC * dC;
+console.log(v);
--- js/01-lesson.js
+++ js/01-lesson.js
@@ -1,25 +1,33 @@
-let myFriendName = "Ploly";
-let number = 10;
-console.log(1);
-console.log(" hello!");
-console.log(number);
+// let myFriendName = "Ploly";
+// let number = 10;
+// console.log(1);
+// console.log(" hello!");
+// console.log(number);
 
- number = 15;
-console.log(number);
+//  number = 15;
+// console.log(number);
 
-const age = 25;
-console.log(age);
+// const age = 25;
+// console.log(age);
 
-const userName = 'Cheisy';
-console.log(userName);
+// const userName = 'Cheisy';
+// console.log(userName);
 
-const isOnline = true;
-console.log(isOnline);
+// const isOnline = true;
+// console.log(isOnline);
 
-const myName = 'veronika';
-console.log(myName);
+// const myName = 'veronika';
+// console.log(myName);
 
-const Age = 15;
-console.log(Age);
+// const Age = 15;
+// console.log(Age);
 
 
+// const answer = prompt ('Чи ви хочете продовжити підписку');
+// console.log(answer);
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+console.log(a1,a2,a3,a4);
