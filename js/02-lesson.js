9a81d41b9cccb43a33629551e5d47cc0da058a1f
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-01-14 13:01:56 +0200

    додає варіанти методів

---
+++ js/02-lesson.js
@@ -0,0 +1,53 @@
+// let width = '50.12px';
+// const result = Number.parseInt(width)
+// console.log(result)
+// const result2 = Number.parseFloat(width)
+// console.log(result2)
+
+// let salary = 1300.16472;
+// const res1 = salary.toFixed(3);
+
+//  console.log(Number(salary.toFixed(3)));
+
+//  const base = 2;
+//  const power = 5;
+//  const res =Math.pow(base, power);
+//  const res3 = base ** power
+//  console.log(res);
+
+// const number = prompt('number');
+//  const power = prompt('stepin');
+//  console.log(Math.pow(number, power))
+
+//  const random = Math.random() * (max - min) + min;
+//  console.log(Math.round(random));
+
+// const message = 'd wmjve hzlre 26 cbvdjksd';
+// console.log(message.length)
+
+// конкатенація
+// const name = 'anna';
+// const lastname = 'lee';
+// const fullname = 'mango' + '' + 'lee';
+// console.log(fullname);
+
+// const firstName = 'Mango';
+// const lastName = 'Lee';
+// const room = 716;
+// const type = 'VIP';
+// const sentense = 'Гість' + firstName + '' + lastName + 'поселяється в' + room +'номер' + type;
+// console.log(sentense);
+
+const blacklistedWord1 = 'спам';
+const blacklistedWord2 = 'розпродаж';
+
+const string1 = 'Привіт, я принц Абдул, це не спам, пропоную тобі мільйон!';
+const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+const string3 = 'Рекламна кампанія #jsIsAwesome';
+
+const norm = string1.toLowerCase();
+const norm2 = string2.toLowerCase();
+const norm3 = string3.toLowerCase();
+
+console.log(norm2.includes(blacklistedWord2));
+console.log(norm3.includes(blacklistedWord2));
