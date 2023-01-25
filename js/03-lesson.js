ca22a59783557d782f5f9f5689239a1df326d20a
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-01-25 13:02:27 +0200

    Update 03-lesson.js

--- js/03-lesson.js
+++ js/03-lesson.js
@@ -100,21 +100,21 @@
  * - В результаті вивести повідомлення
  * «Оформляемо замовлення на суму [сума] зі знижкою [знижка]%»
  */
-const totalSpent = 2000;
-let payment = 500;
-let discount = 0;
-if(totalSpent>100 && totalSpent<1000){
-    discount=0.02
-    console.log(`бронзовий партнер, знижка ${discount*100}%`)
-}
-else if(totalSpent>1000 && totalSpent<5000){
-    discount=0.05
-    console.log(`срібний партнер, знижка ${discount*100}%`)
-}
-else if(totalSpent>=5000){
-    discount=0.10
-    console.log(`золотий партнер, знижка ${discount*100}%`)
-}
+// const totalSpent = 2000;
+// let payment = 500;
+// let discount = 0;
+// if(totalSpent>100 && totalSpent<1000){
+//     discount=0.02
+//     console.log(`бронзовий партнер, знижка ${discount*100}%`)
+// }
+// else if(totalSpent>1000 && totalSpent<5000){
+//     discount=0.05
+//     console.log(`срібний партнер, знижка ${discount*100}%`)
+// }
+// else if(totalSpent>=5000){
+//     discount=0.10
+//     console.log(`золотий партнер, знижка ${discount*100}%`)
+// }
 
 
 
