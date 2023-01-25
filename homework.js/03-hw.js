8ff8ba8f6975eea0393806f1a43e6fc1d5c84127
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-01-26 00:33:47 +0200

    додає дз

---
+++ homework.js/03-hw.js
@@ -0,0 +1,28 @@
+// const number = prompt ("Введіть число");
+// if(number>55 && number<100){
+//     console.log("Число потрапляє в діапазон")
+// }else{
+//     console.log("Число не потрапляє в діапазон")
+// }
+
+
+// const maxim = 55;
+// if(maxim>0 && maxim<16){
+//     console.log("Діти")
+// }
+// else if(maxim>17 && maxim<60){
+//     console.log("Дорослі")
+// }
+// else if(maxim>61 && maxim<100){
+//     console.log("Пенсіонери")
+// }
+
+// const myname =" Veronika";
+// const lastname = "Niema";
+// if(myname.length>4 && lastname.length>5){
+//     const result = myname.length + lastname.length;
+//     console.log(result)
+// }else{
+//     console.log("УПС")
+// }
+
