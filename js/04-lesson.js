8815d0dbb486a66c3cc981e3e85128d346a3b695
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-01-28 13:49:31 +0200

    додає 4 урок

---
+++ js/04-lesson.js
@@ -0,0 +1,62 @@
+// for(let i=0; i <= 5; i += 1){
+//   console.log(i);
+// }
+
+/*
+ * Напиши скрипт який підраховує загальну суму зп працівників.
+ * Кількість робітників зберігається в змінній employees.
+ * ЗП кожного працівника це випадкове число від 500 до 5000
+ * Записати суму в змінну totalSalary і вивести в консоль
+ */
+
+// const employees = 4;
+// const minSalary = 500;
+// const maxSalary = 500;
+// let totalSalary = 0;
+// for(let i = 0; i < 4; i += 1){
+//     const salary = Math.round(Math.random() * (maxSalary - minSalary ) + minSalary);
+//     console.log(`salary ${i}:`, salary);
+//     totalSalary += salary
+
+//     console.log(`totalSalary ${i}:`,totalSalary);
+// }
+
+/*
+ * Напиши скрипт який порахує суму всіх парних чисел,
+ * які входять в діапазон чисел в змінних від min до max.
+ * Наприклад, якщо min=0 и max=5, то діапазон 0-5, і в 
+ * ньому 2 парних числа - 2 та 4, їх сума 6.
+ */
+
+// 1.Створюємо змінні
+// 2.Створюємо цикл
+// 3.Перевірка на парність числа
+// 4. Плюсуємо числа
+
+// const min = 0;
+// const max =5;
+// let total = 0;
+// for(let i = min; i <= max; i += 1){
+//   if(i % 2 === 0){
+//     total += i
+//   }
+//   console.log(total)
+// }
+
+// let number;
+// while(number !== 5){
+//     if(number === 0){
+//         break;
+//     }
+
+//    number = Number(prompt('Введіть число 5'));
+//     console.log(number);
+// }
+
+// for(let i=0; i <= 10; i += 1){
+//   console.log(i);
+// }
+
+// 4. Написати скрипт який буде перевіряти середній бал успішності учня.
+// Прервати роботу циклу, коли середній бал буде меньше 7.
+ 
