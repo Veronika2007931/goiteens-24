b23b7913080b95b219c55de8504d5864f7cbd3e6
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-01-29 22:45:02 +0200

    додає дз4

---
+++ homework.js/04-hw.js
@@ -0,0 +1,41 @@
+// for(let i=0; i <= 10; i += 1){
+//       console.log(i);
+//     }
+
+// for(let i=0; i <= 10; i += 1){
+//           console.log(i);
+//    if(i % 2 === 0){
+//     console.log('Число парне')
+//    }else {
+//     console.log("Число непарне")
+//    }
+
+// }
+
+// let num = 20;
+// while(num <= 28){
+//     console.log(num)
+//     num += 1
+// }
+
+let heroName = '';
+while(heroName.length < 6){
+    heroName = prompt("Введіть ім'я героя")
+    console.log(heroName)
+    if(heroName >= 6){
+        // console.log('no')
+        break;
+    }
+    }
+
+   
+ 
+
+ 
+   
+ 
+
+
+
+
+
