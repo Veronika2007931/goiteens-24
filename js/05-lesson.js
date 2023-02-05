54365edf71e7ef51ef9ef978160054ff7d8b1ccc
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-02-05 23:36:57 +0200

    зміни

--- js/05-lesson.js
+++ js/05-lesson.js
@@ -23,20 +23,20 @@
 // const titleJoin = titleArr.join('-');
 // console.log(titleJoin)
 
-// const titleSlug = title.toLocaleLowerCase().split('').join('-');
+// const titleSlug = title.toLocaleLowerCase().split(' ').join('-');
 // console.log(titleSlug)
 
-const cards = [
-    'Карточка-1',
-    'Карточка-2',
-    'Карточка-3',
-    'Карточка-4',
-    'Карточка-5',
-  ];
+// const cards = [
+//     'Карточка-1',
+//     'Карточка-2',
+//     'Карточка-3',
+//     'Карточка-4',
+//     'Карточка-5',
+//   ];
   /*
    * Видалення (по індексу), метод indexOf()
    */
-  const cardToRemove = 'Карточка-3';
+  // const cardToRemove = 'Карточка-3';
 // const index = cards.indexOf(cardToRemove);
 // console.log(index);
 // cards.splice(index, 1)
@@ -46,15 +46,16 @@ const cards = [
    * Додавання (по індексу)
    */
   
-const cardToInsert = 'Карточка-6';
+// const cardToInsert = 'Карточка-6';
 
 // cards.splice(6,0,cardToInsert);
 // console.log(cards);
 // 
 //    * Оновлення (по індексу)
 //    */
-  const cardToUpdate = 'Карточка-4';
-  const idx = cards.indexOf(cardToUpdate)
+  // const cardToUpdate = 'Карточка-4';
+  // const idx = cards.indexOf(cardToUpdate)
 
-  cards.splice(idx, 1, cardToInsert  )
-  console.log(cards)
+  // cards.splice(idx, 1, cardToInsert  )
+  // console.log(cards)
+
