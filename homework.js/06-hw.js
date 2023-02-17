4e1a373a8d94d940a58120402a88ab57b03a478c
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-02-17 22:00:53 +0200

    add 6 hw

--- homework.js/06-hw.js
+++ homework.js/06-hw.js
@@ -3,7 +3,7 @@ const hello1 = function(){
    
 }
 
-hello1();
+// hello1();
 
 const hello2 = function(name){
     console.log(`Привіт,${name}`);
@@ -28,5 +28,26 @@ const myNewArrowFunction = (...args) => console.log(args)
 // myNewArrowFunction(100, 200, 300, 400, 500);
 // myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
 
+const myAverageScore = function(arr){
+     let totalScore = 0;
+   for(let element of arr){
+      totalScore += element;
+    
+   }
+ totalScore = totalScore /  arr.length
+if(totalScore<=100 && totalScore>=91){
+   return 'Myaveragescore: A ';
+}else if(totalScore<=90 && totalScore>=81){
+    return 'Myaveragescore: B ';
+}else if(totalScore<=80 && totalScore>=71){
+    return 'Myaveragescore: C ';
+}else if(totalScore<=70){
+    return 'Myaveragescore: D ';
+}
+}
 
 
+console.log(myAverageScore([100, 75, 81, 96]));
+console.log(myAverageScore([45, 63, 85, 70]));
+console.log(myAverageScore([77, 82, 60, 58]));
+console.log(myAverageScore([93, 99, 93, 96]));
