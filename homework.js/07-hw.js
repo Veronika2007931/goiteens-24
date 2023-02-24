02eddf2415f714d90c9e80d32d3491f4da2738a2
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-02-24 23:09:52 +0200

    додає дз 7

---
+++ homework.js/07-hw.js
@@ -0,0 +1,88 @@
+// const user = {
+//     userName: 'Veronika',
+//     age: 17,
+//     showUserName(){
+//       console.log(this.userName)
+//     },
+//     updateAge(){
+//         const values = Object.values(user);
+//         const result = values[1]+1;
+//         console.log(result)  
+//     }
+// }
+
+
+// user.showUserName();
+// user.updateAge();
+
+// const salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//     }
+
+// total = 0;
+    // const values = Object.values(salaries);
+   
+    // for (const element of values){
+    // total += element
+    //    }
+    //  let sum  = total
+    // console.log(sum);
+
+const calculator = {
+    a: prompt('Add a number'),
+    b: prompt('Add a number'),
+    // read(a,b){
+      
+    // },
+    sum(){
+        const values = Object.values(calculator);
+       return values[0]+values[1];
+    },
+    mult(){
+        a*b
+    }
+}
+
+
+// calculator.read()
+console.log(calculator.sum())
+// calculator.mult()
+
+// const cafe= {
+//     name: 'Republick',
+//     width:200,
+//     height: 300,
+//     personal:5,
+//     hr: true }
+
+//     const entries = Object.entries(cafe);
+// console.log(entries);
+
+// const values = Object.values(cafe);
+// console.log(values)
+//  const result = values[1]*values[2];
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/07-lesson.js
+++ js/07-lesson.js
@@ -1,82 +1,82 @@
-// const class1 = {
-//     name: '10-A',
-//     students: ['Vlad', 'Fedir', 'Nastia', 'Matvii'],
-//     studentCount:4,
-//     getName(){
+const class1 = {
+    name: '10-A',
+    students: ['Vlad', 'Fedir', 'Nastia', 'Matvii'],
+    studentCount:4,
+    getName(){
 
-//         console.log(this.name)
-//     },
+        console.log(this.name)
+    },
     
-//     changeName(newName){
-//       return this.name = newName
-//     },
-//     getTracksLength(){
-//         return this.students.lenght;
-//     },
-//     getNewName(newName){
-//         this.students.push(newName);
-//     }
+    changeName(newName){
+      return this.name = newName
+    },
+    getTracksLength(){
+        return this.students.lenght;
+    },
+    getNewName(newName){
+        this.students.push(newName);
+    }
     
-// }
+}
 
-// class1.getName();
-// class1.changeName('vvhhbjbgik')
+class1.getName();
+class1.changeName('vvhhbjbgik')
 
-// console.log(class1.name)
+console.log(class1.name)
 
-// // Це щоб змінювати значення в об'єкті нижче
-// class1.name = '10-B'
-// console.log(class1)
+// Це щоб змінювати значення в об'єкті нижче
+class1.name = '10-B'
+console.log(class1)
 
-// // Додання нової властивості нижче
-// class1.raiting = 10;
-// console.log(class1['raiting'])
+// Додання нової властивості нижче
+class1.raiting = 10;
+console.log(class1['raiting'])
 
-// class1.students.push('Yelisey')
+class1.students.push('Yelisey')
 
-// // якщо трреба видалити якісь дані
-// delete class1.raitihg
+// якщо трреба видалити якісь дані
+delete class1.raitihg
 
-// // Якщо і ключ і властивість 
+// Якщо і ключ і властивість 
 // називаються однаково то можна записувати без кдюча
-// const signupData = {
-//     userName,
-//     email,
-// }
+const signupData = {
+    userName,
+    email,
+}
 
-// const arr = [1, 2, 3, 4];
-// console.log(arr)
+const arr = [1, 2, 3, 4];
+console.log(arr)
 
-// arr.greats= 'hello'
-// console.log(arr)
+arr.greats= 'hello'
+console.log(arr)
 
-// const feedback = {
-//     good: 5,
-//     neutral: 10,
-//     bad:3,
-// }
+const feedback = {
+    good: 5,
+    neutral: 10,
+    bad:3,
+}
 
-// const allkeys = Object.keys(feedback);
-// console.log(allkeys);
+const allkeys = Object.keys(feedback);
+console.log(allkeys);
 
-// let total =0;
-// for (const key of allkeys){
-//     console,log(feedback[key]);
-//     total += feedback[key]
+let total =0;
+for (const key of allkeys){
+    console.log(feedback[key]);
+    total += feedback[key]
 
-// }
+}
 
-// const values = Object.values(feedback);
-// console.log(values);
+const values = Object.values(feedback);
+console.log(values);
 
-// for (const value of values) {
-//     console.log(value);
-//     total += value;
+for (const value of values) {
+    console.log(value);
+    total += value;
 
-// }
+}
 
-// const entries = Object.entries(feedback)
-// console.log(entries)
+const entries = Object.entries(feedback)
+console.log(entries)
 
 const friends = [
     { name: 'Mango', online: false },
@@ -84,18 +84,18 @@ const friends = [
     { name: 'Poly', online: false },
     { name: 'Ajax', online: false },
   ];
-// const findFriendByName = function(allFriends, nameToFind){
-//     for (const friend of allFriends) {
-//         if(friend.name === nameToFind){
-//             console.log('знайшли друга')
-//             return nameToFind;
-//         }else{
-//             console.log('такого друга не існує')
-//         }
-//     }
-// }
-//  console.log(findFriendByName(friends, 'Poly'));
-//   console.log(findFriendByName(friends, 'Chelsy'));
+const findFriendByName = function(allFriends, nameToFind){
+    for (const friend of allFriends) {
+        if(friend.name === nameToFind){
+            console.log('знайшли друга')
+            return nameToFind;
+        }else{
+            console.log('такого друга не існує')
+        }
+    }
+}
+ console.log(findFriendByName(friends, 'Poly'));
+  console.log(findFriendByName(friends, 'Chelsy'));
 
 const getAllNames = function(allFriends){
   const allNames = [];
