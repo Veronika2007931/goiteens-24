bb33bedc4e8191ca5f839ad22028bca47077b7b4
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-03-30 23:47:09 +0300

    add 12 hw

---
+++ homework.js/12-hw.js
@@ -0,0 +1,42 @@
+
+// #1
+const lists = document.querySelectorAll('.item')
+const listLength = lists.length
+console.log(`У списку ${listLength} категорій`)
+
+c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
