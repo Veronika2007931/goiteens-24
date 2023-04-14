d11e84b8c9be2ec2c6d3c81a19ec640aa0d87c5e
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-04-14 23:38:01 +0300

    add hw 14

---
+++ homework.js/14-hw.js
@@ -0,0 +1,66 @@
+// №5
+const inputEl = document.querySelector('input')
+const spanEl = document.querySelector('#name-output')
+
+const value = inputEl.value
+console.log(value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// №8
+// const btnEl = document.querySelector('[data-action="render"]')
+// const buttonEl = document.querySelector('[data-action="destroy"]')
+// const inputEl = document.querySelector('input')
+// const boxes = document.querySelector('#boxes')
+
+// btnEl.addEventListener('click', onBtnCreateClick)
+// buttonEl.addEventListener('click', onBtnDestroyClick)
+
+// function onBtnCreateClick(){
+//     const inputValue = inputEl.value;
+//     createBoxes(inputValue)
+    
+   
+// }
+
+// function createBoxes(amount){
+//     let boxSize = 30;
+//     for(let i=0; i< amount; i+= 1){
+//         const newDiv = document.createElement('div')
+//         newDiv.style.width = boxSize + 'px';
+//         newDiv.style.height = `${boxSize}px`;
+//         let color = Math.round(Math.random()*65025).toString(16);
+//         newDiv.style.backgroundColor ='#' + color;
+//         boxes.append(newDiv)
+//         boxSize += 10;
+//     }
+// }
+
+// function onBtnDestroyClick(){
+//     boxes.innerHTML = '';
+// }
+
+
+
+
+
